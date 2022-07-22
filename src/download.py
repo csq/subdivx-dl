ebc4c4bcf689fb4f34be5281fbfe24f22b160fb0
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-07-22 10:52:52 -0300

    fix: error don´t wait to finish download file

--- src/download.py
+++ src/download.py
@@ -1,11 +1,11 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen
 import zipfile
 
 def downloadFile(url):
   location = '.temp/'
   args = ['wget', '--content-disposition', '-q', '-c', '-P', location, url]
-  output = Popen(args, stdout=PIPE)
-  #stdout = output.communicate()
+  output = Popen(args)
+  output.wait()
 
 def unzip(fileZip):
    extension = '.srt'
