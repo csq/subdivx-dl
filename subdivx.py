62695530a3e9cb7d9a826d64339b9071518d9da8
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-07-28 15:16:07 -0300

    refactor: separe code in functions

--- subdivx.py
+++ subdivx.py
@@ -22,114 +22,165 @@ if ((len(sys.argv)-1) == 0):
 else:
 	FIND_SUBTITLE = sys.argv[1]
 
-payload = {
-    "buscar2": FIND_SUBTITLE,
-    "accion": "5",
-    "masdesc": "",
-    "subtitulos": "1",
-    "realiza_b": "1",
-}
-
 http = urllib3.PoolManager()
-request = http.request('POST', SUBDIVX_MAIN_URL, fields=payload)
 
-page = BeautifulSoup(request.data, 'html.parser')
+def getDataPage(poolManager, url, search):
 
-results_descriptions = page.find_all('div', id='buscador_detalle_sub')
-results_url = page.find_all('a', {'class': 'titulo_menu_izq'})
-results_downloads = page.find_all('div', id='buscador_detalle_sub_datos')
-results_user = page.find_all('a', {'class': 'link1'})
+	payload = {
+	    "buscar2": search,
+	    "accion": "5",
+	    "masdesc": "",
+	    "subtitulos": "1",
+	    "realiza_b": "1",
+	}
 
-if not results_descriptions:
-	print('No suitable subtitles')
-	sys.exit(0)
+	request = http.request('POST', url, fields=payload)
+	page = BeautifulSoup(request.data, 'html.parser')
 
-tittleList = list()
-descriptionList = list()
-urlList = list()
-downloadList = list()
-userList = list()
-dateList = list()
+	results_descriptions = page.find_all('div', id='buscador_detalle_sub')
+	results_url = page.find_all('a', {'class': 'titulo_menu_izq'})
+	results_downloads = page.find_all('div', id='buscador_detalle_sub_datos')
+	results_user = page.find_all('a', {'class': 'link1'})
 
-separator = "Subtitulos de "
-for tittle in results_url:
-	string = tittle.get_text()
-	tittleList.append(string[len(separator):])
+	if not results_descriptions:
+		print('No suitable subtitles')
+		sys.exit(0)
 
-sizeText = 100
-for description in results_descriptions:
-	text = description.get_text()
-	shortText = text[:sizeText]
-	descriptionList.append(shortText)
+	tittleList = list()
+	descriptionList = list()
+	urlList = list()
+	downloadList = list()
+	userList = list()
+	dateList = list()
 
-for link in results_url:
-	urlList.append(link.get('href'))
+	separator = "Subtitulos de "
+	for tittle in results_url:
+		string = tittle.get_text()
+		tittleList.append(string[len(separator):])
 
-patternNumber = '\d+(?:\,\d+)?'
-patternDate = '(\d+/\d+/\d+)'
-sizeText = 20
-for download in results_downloads:
-	text = download.get_text()
+	for description in results_descriptions:
+		text = description.get_text()
+		descriptionList.append(text)
 
-	date = re.search(patternDate, text)
-	if date != 'null':
-		dateList.append(date.group())
+	for link in results_url:
+		urlList.append(link.get('href'))
 
-	subText = text[:sizeText]
-	downloadCount = re.search(patternNumber, subText)
+	patternNumber = '\d+(?:\,\d+)?'
+	patternDate = '(\d+/\d+/\d+)'
+	sizeText = 20
+	for download in results_downloads:
+		text = download.get_text()
 
-	if downloadCount != 'null':
-		downloadList.append(downloadCount.group())
+		date = re.search(patternDate, text)
+		if date != 'null':
+			dateList.append(date.group())
 
-banWord = ["tÃ­tulo", "fecha", "downloads", "subtitulos en espaÃ±ol"]
-for user in results_user:
-	userName = user.get_text()
-	if userName not in banWord:
-		userList.append(userName)
+		subText = text[:sizeText]
+		downloadCount = re.search(patternNumber, subText)
 
-# Table (id, tittle, description, downloads, date, user)
-df = pd.DataFrame({'Tittle':tittleList, 'Description':descriptionList, 'Downloads':downloadList, 'Date':dateList, 'User':userList})
+		if downloadCount != 'null':
+			downloadList.append(downloadCount.group())
 
-# Displaying the DataFrame
-print(tabulate(df, headers = 'keys', tablefmt = 'pretty'))
+	banWord = ["tÃ­tulo", "fecha", "downloads", "subtitulos en espaÃ±ol"]
+	for user in results_user:
+		userName = user.get_text()
+		if userName not in banWord:
+			userList.append(userName)
 
-try:
-	selection = int(input('\n[Selection] : '))
-	request = http.request('GET', urlList[selection])
-except ValueError:
-	sys.exit('Input only numbers')
-except IndexError:
-    sys.exit('input valid numbers')
+	return tittleList, descriptionList, urlList, downloadList, userList, dateList
 
-# Scrap page download srt
-page = BeautifulSoup(request.data, 'html.parser')
-urlFile = page.find('a', {'class': 'link1'})
+def printSelectDescription(selection, descriptionList):
+	description_select = []
+	aux = descriptionList[selection].splitlines()
+	words = aux[0].split()
 
-urlFileToDownload = SUBDIVX_URL + urlFile.get('href')
+	maxLengh =  10
+	line = ""
+	count = 0
 
-downloadFile(urlFileToDownload)
+	for word in words:
+		if (count <= maxLengh):
+			line = line+" "+word
+			count = count + 1
+		else:
+			a = line+" "+word
+			description_select.append(a)
+			line = ""
+			count = 0
+	description_select.append(line)
+	df_description = pd.DataFrame({'Description':description_select})
+	print(tabulate(df_description, headers = 'keys', tablefmt = 'pretty', stralign='left'))
 
-# Clear screen
-os.system('clr' if os.name == 'nt' else 'clear')
+def getSubtitle(request, url):
+	# Scrap page download srt
+	page = BeautifulSoup(request.data, 'html.parser')
+	urlFile = page.find('a', {'class': 'link1'})
 
-fpath = os.path.join(os.getcwd(), '.temp', '')
-listDirectory = os.listdir(fpath)
+	urlFileToDownload = url + urlFile.get('href')
 
-for file in listDirectory:
-	pathFile = os.path.join(fpath, file)
+	downloadFile(urlFileToDownload)
 
-	if (file.endswith('.zip')):
-		unzip(pathFile)
-	elif (file.endswith('.rar')):
-		unrar(pathFile)
+	# Clear screen
+	clear()
 
-# Remove temp folder
-try:
-	shutil.rmtree(fpath)
-	print('Clear temp files\n')
-except OSError as error:
-	print(error)
+	fpath = os.path.join(os.getcwd(), '.temp', '')
+	listDirectory = os.listdir(fpath)
 
-# Rename bulk srt
-currentPath = os.getcwd()
-renameFile(currentPath, FIND_SUBTITLE)
+	for file in listDirectory:
+		pathFile = os.path.join(fpath, file)
+
+		if (file.endswith('.zip')):
+			unzip(pathFile)
+		elif (file.endswith('.rar')):
+			unrar(pathFile)
+
+	# Remove temp folder
+	try:
+		shutil.rmtree(fpath)
+		print('Clear temp files\n')
+	except OSError as error:
+		print(error)
+
+	# Rename bulk srt
+	currentPath = os.getcwd()
+	renameFile(currentPath, FIND_SUBTITLE)
+
+def clear():
+	os.system('clr' if os.name == 'nt' else 'clear')
+
+tittleList, descriptionList, urlList, downloadList, userList, dateList = getDataPage(http, SUBDIVX_URL, FIND_SUBTITLE)
+
+# Table (id, tittle, downloads, date, user)
+df = pd.DataFrame({'Tittle':tittleList, 'Downloads':downloadList, 'Date':dateList, 'User':userList})
+
+while (1):
+	# Clear screen
+	clear()
+	
+	# Display Search Results
+	print(tabulate(df, headers = 'keys', tablefmt = 'pretty'))
+
+	try:
+		selection = int(input('\n[Selection] : '))
+		request = http.request('GET', urlList[selection])
+	except ValueError:
+		sys.exit('Input only numbers')
+	except IndexError:
+	    sys.exit('input valid numbers')
+
+	clear()
+	printSelectDescription(selection, descriptionList)
+
+	print('\n[1] Download subtitle')
+	print('[2] Back\n')
+
+	try:
+		select_action = int(input('[Selection] : '))
+	except ValueError:
+		sys.exit('Input only numbers')
+
+	if select_action == 1:
+		getSubtitle(request, SUBDIVX_URL)
+		exit(0)
+	elif select_action == 2:
+		clear()
