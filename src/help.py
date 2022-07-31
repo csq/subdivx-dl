3ff04b5491903c7b8717addb9dd821c9837fc1b3
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-07-31 20:56:20 -0300

    refactor: text alignment in help messages

--- src/help.py
+++ src/help.py
@@ -1,11 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-	description='Subdvix-dl is a subtitle downloader of website subdvix.com',
-	epilog='Disclaimer: subdvix.com not involve in this development.\
-	        Any comments about this software make it in: \
-	        <www.github.com/csq/subdivx-dl> or \
-	        <www.gitlab.com/csq1/subdivx-dl>'
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='Subdvix-dl is a subtitle downloader of website subdvix.com',
+    epilog='''Disclaimer: subdvix.com not involve in this development.\n
+           \rAny comments about this software make it in:\n \
+	       \r<www.github.com/csq/subdivx-dl> or <www.gitlab.com/csq1/subdivx-dl>'''
 )
 
 parser.add_argument('SEARCH', help='Name at serie or movie to search subtitle')
