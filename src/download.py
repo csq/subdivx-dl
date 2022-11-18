ecdb0ccfd52fe2576c621def7420c84bc6498a19
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-11-17 23:37:55 -0300

    fix: error causing existing files to be renamed in the same destination folder

--- src/download.py
+++ src/download.py
@@ -3,6 +3,7 @@ import zipfile
 import os
 
 location = '.temp/'
+temp_dir = os.path.join(os.getcwd(), location, '')
 
 def downloadFile(url):
   args = ['wget', '--content-disposition', '-q', '-c', '-P', location, url]
@@ -15,7 +16,7 @@ def unzip(fileZip):
        with zipfile.ZipFile(fileZip, 'r') as z:
             for file in z.namelist():
                  if file.endswith(extension):
-                      z.extract(file)
+                      z.extract(file, temp_dir)
             print('Extraction sucessfull subtittle enjoy!\n', file)
    except:
        print('Invalid file')
@@ -23,7 +24,7 @@ def unzip(fileZip):
 def unrar(fileRar):
     devnull = open('/dev/null', 'w')
 
-    args = ['unrar', 'x', fileRar]
+    args = ['unrar', 'x', fileRar, temp_dir]
     sp = Popen(args, stdout=devnull)
     sp.wait()
 
@@ -37,10 +38,11 @@ def renameFile(pathFile, newName):
         if (files[index].endswith('.srt')):
             old_name = os.path.join(pathFile, files[index])
             if (count == 0):
-                new_name = os.path.join(pathFile, f'{newName}.srt')
+                new_name = os.path.join(os.getcwd(), f'{newName}.srt')
                 os.rename(old_name, new_name)
                 count = count + 1
             else:
-                new_name = os.path.join(pathFile, f'{newName}-V{count}.srt')
+                new_name = os.path.join(os.getcwd(), f'{newName}-V{count}.srt')
                 os.rename(old_name, new_name)
+                count = count + 1
         index = index + 1
