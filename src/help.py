67eaad98adf48f9b62a98c6b0504ee94d6f630aa
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-11-25 17:46:55 -0300

    fix: user messages

--- src/help.py
+++ src/help.py
@@ -1,14 +1,14 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-	formatter_class=argparse.RawDescriptionHelpFormatter,
-    description='Subdvix-dl is a subtitle downloader of website subdvix.com',
-    epilog='''Disclaimer: subdvix.com not involve in this development.\n
-           \rAny comments about this software make it in:\n \
+       formatter_class=argparse.RawDescriptionHelpFormatter,
+       description='Subdvix-dl is a subtitle downloader for the website subdvix.com',
+       epilog='''Disclaimer: subdvix.com not involve in this development.\n
+              \rAny comments about this software make it in:\n \
 	       \r<www.github.com/csq/subdivx-dl> or <www.gitlab.com/csq1/subdivx-dl>'''
 )
 
-parser.add_argument('SEARCH', help='Name at serie or movie to search subtitle')
-parser.add_argument('-v','--version', action='version', version='2022.07.31')
+parser.add_argument('SEARCH', help='name of the tv serie or movie to search for subtitle')
+parser.add_argument('-v', '--version', action='version', version='2022.07.31')
 parser.add_argument('-s', '--season', help='download full season subtitles', action='store_true')
-parser.add_argument('-l', '--location', help='target directory')
+parser.add_argument('-l', '--location', help='destination directory')
