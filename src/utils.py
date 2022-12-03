7b56b99350b9dbbccb0dd24ad237cb0c8ef67dd1
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-12-03 20:49:33 -0300

    refactor: unified utils code

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import zipfile
 import shutil
@@ -8,6 +9,22 @@ import re
 from subprocess import Popen
 from bs4 import BeautifulSoup
 from tabulate import tabulate
+
+# Parser for command-line
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='Subdvix-dl is a subtitle downloader for the website subdvix.com',
+    epilog='''Disclaimer: subdvix.com not involve in this development.\n
+            \rAny comments about this software make it in:\n \
+            \r<www.github.com/csq/subdivx-dl> or <www.gitlab.com/csq1/subdivx-dl>'''
+)
+
+parser.add_argument('SEARCH', help='name of the tv serie or movie to search for subtitle')
+parser.add_argument('-v', '--version', action='version', version='2022.07.31')
+parser.add_argument('-s', '--season', help='download full season subtitles', action='store_true')
+parser.add_argument('-l', '--location', help='destination directory')
+parser.add_argument('--order-by-downloads', help='order results by downloads', action='store_true')
+parser.add_argument('--order-by-dates', help='order results by dates', action='store_true')
 
 # Create and configure logger
 logging.basicConfig(
