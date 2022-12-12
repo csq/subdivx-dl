0ef12c241a0466d0de2e01da3dd64707dbcc6715
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-12-12 17:11:18 -0300

    update: version tag

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='subdivx-dl',
-    version='2022.07.31',
+    version='2022.12.12',
     url='https://github.com/csq/subdivx-dl',
     license='GPLv3+',
     description='A subtitle downloader for the website subdvix.com',
