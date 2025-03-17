f5f68111f99afca707791155e5c88311c9c3e62b
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2025-03-17 11:27:03 -0300

    style: replace tabs with spaces

--- subdivx_dl/main.py
+++ subdivx_dl/main.py
@@ -14,7 +14,7 @@ FIND_SUBTITLE = parse_user_input(args.SEARCH)
 rev = get_random_rev()
 
 headers = {
-	'user-agent': f'Mozilla/5.0 (X11; Linux x86_64; rv:{rev}) Gecko/20100101 Firefox/{rev}'
+    'user-agent': f'Mozilla/5.0 (X11; Linux x86_64; rv:{rev}) Gecko/20100101 Firefox/{rev}'
 }
 
 # Create a PoolManager instance for HTTPS requests
@@ -22,8 +22,8 @@ https = urllib3.PoolManager(
     headers=headers,
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where(),
-	timeout=5,
-	retries=3
+    timeout=5,
+    retries=3
 )
 
 # Create a DataClient instance
@@ -31,156 +31,156 @@ data_client = DataClient(https, headers, SUBDIVX_URL)
 
 # Load or generate data session
 if not data_client.does_data_exist() or data_client.does_data_session_expire():
-	data_client.generate_data()
-	data_client.save_data()
+    data_client.generate_data()
+    data_client.save_data()
 
 data_session = data_client.get_data_session()
 
 # Save or load configuration
 if args.save_config:
-	Config().save_config(args)
+    Config().save_config(args)
 elif args.load_config:
-	config = Config().load_config()
-	args = Args(args, config)
+    config = Config().load_config()
+    args = Args(args, config)
 else:
-	args = Args(args)
+    args = Args(args)
 
 def main():
-	# Get all data from search
-	search_data = get_data_page(args, https, SUBDIVX_URL, data_session, FIND_SUBTITLE)
+    # Get all data from search
+    search_data = get_data_page(args, https, SUBDIVX_URL, data_session, FIND_SUBTITLE)
 
-	# Sorting data if flag is set
-	if args.order_by_downloads or args.order_by_dates:
-		search_data = sort_data(args, search_data)
+    # Sorting data if flag is set
+    if args.order_by_downloads or args.order_by_dates:
+        search_data = sort_data(args, search_data)
 
-	# Save a copy of the original data
-	search_data_complete = search_data
+    # Save a copy of the original data
+    search_data_complete = search_data
 
-	# Limit the number of results displayed based on the user's preference
-	if args.lines:
-		result_limit = args.lines
-	else:
-		base_limit = 2 if args.compact else 3 if args.alternative else 5 if args.style and args.style.endswith('grid') else 10
-		result_limit = base_limit + (1 if args.disable_help else 0)
+    # Limit the number of results displayed based on the user's preference
+    if args.lines:
+        result_limit = args.lines
+    else:
+        base_limit = 2 if args.compact else 3 if args.alternative else 5 if args.style and args.style.endswith('grid') else 10
+        result_limit = base_limit + (1 if args.disable_help else 0)
 
-	search_data = search_data[:result_limit]
+    search_data = search_data[:result_limit]
 
-	# Checking flag for switch to fast download mode
-	if args.fast:
-		id_subtitle = get_best_match(args, search_data_complete)
-		get_subtitle(args, https, SUBDIVX_URL, id_subtitle)
-		exit(0)
+    # Checking flag for switch to fast download mode
+    if args.fast:
+        id_subtitle = get_best_match(args, search_data_complete)
+        get_subtitle(args, https, SUBDIVX_URL, id_subtitle)
+        exit(0)
 
-	# Initialize cache for comments
-	if args.comments:
-		cache_comments = TTLCache(capacity=len(search_data), ttl=120)
+    # Initialize cache for comments
+    if args.comments:
+        cache_comments = TTLCache(capacity=len(search_data), ttl=120)
 
-	# Pagination
-	current_index = 0
-	block_size = (result_limit if args.lines is None else args.lines)
+    # Pagination
+    current_index = 0
+    block_size = (result_limit if args.lines is None else args.lines)
 
-	# Get the size of the complete data
-	search_data_complete_size = len(search_data_complete)
+    # Get the size of the complete data
+    search_data_complete_size = len(search_data_complete)
 
-	while True:
-		# Clear screen
-		clear()
+    while True:
+        # Clear screen
+        clear()
 
-		# Slice data
-		search_data = search_data_complete[current_index:current_index + block_size]
+        # Slice data
+        search_data = search_data_complete[current_index:current_index + block_size]
 
-		# Show Search Results
-		if args.compact:
-			print_search_results_compact(args, search_data)
-		else:
-			print_search_results(args, search_data)
+        # Show Search Results
+        if args.compact:
+            print_search_results_compact(args, search_data)
+        else:
+            print_search_results(args, search_data)
 
-		# Get the user selection
-		if search_data_complete_size > block_size:
+        # Get the user selection
+        if search_data_complete_size > block_size:
 
-			# Show the pagination
-			page_info = get_pagination_info(search_data_complete_size, block_size, current_index)
-			page_info_format = f'[{page_info["current_page"]}/{page_info["total_pages"]}]'
-			print(page_info_format.center(get_terminal_width()))
+            # Show the pagination
+            page_info = get_pagination_info(search_data_complete_size, block_size, current_index)
+            page_info_format = f'[{page_info["current_page"]}/{page_info["total_pages"]}]'
+            print(page_info_format.center(get_terminal_width()))
 
-			user_input = prompt_user_selection(args, 'pagination')
-		else:
-			user_input = prompt_user_selection(args, 'subtitle')
+            user_input = prompt_user_selection(args, 'pagination')
+        else:
+            user_input = prompt_user_selection(args, 'subtitle')
 
-		try:
-			selection = int(user_input) - 1
-			id_subtitle = str(search_data[selection]['id_subtitle'])
-		except (ValueError, IndexError):
-			if user_input.lower() == 'n':
-				if current_index >= search_data_complete_size:
-					current_index = search_data_complete_size - block_size
-				elif current_index + block_size < search_data_complete_size:
-					current_index += block_size
-			elif user_input.lower() == 'p':
-				current_index -= block_size
-				if current_index < 0:
-					current_index = 0
-			else:
-				print('\nInput only numbers')
-				delay(0)
-				continue
-			clear()
-			continue
+        try:
+            selection = int(user_input) - 1
+            id_subtitle = str(search_data[selection]['id_subtitle'])
+        except (ValueError, IndexError):
+            if user_input.lower() == 'n':
+                if current_index >= search_data_complete_size:
+                    current_index = search_data_complete_size - block_size
+                elif current_index + block_size < search_data_complete_size:
+                    current_index += block_size
+            elif user_input.lower() == 'p':
+                current_index -= block_size
+                if current_index < 0:
+                    current_index = 0
+            else:
+                print('\nInput only numbers')
+                delay(0)
+                continue
+            clear()
+            continue
 
-		if selection < -1:
-			print('\nInput only positive numbers')
-			delay(0)
-			continue
-		elif selection == -1:
-			clear()
-			exit(0)
+        if selection < -1:
+            print('\nInput only positive numbers')
+            delay(0)
+            continue
+        elif selection == -1:
+            clear()
+            exit(0)
 
-		clear()
+        clear()
 
-		if args.alternative or args.compact:
-			print_summary(args, selection, search_data)
-		else:
-			print_description(args, selection, search_data)
+        if args.alternative or args.compact:
+            print_summary(args, selection, search_data)
+        else:
+            print_description(args, selection, search_data)
 
-		# Checking flag for add comments view
-		comments_selection = None
+        # Checking flag for add comments view
+        comments_selection = None
 
-		if args.comments:
-			comment_from_cache = cache_comments.get(id_subtitle)
+        if args.comments:
+            comment_from_cache = cache_comments.get(id_subtitle)
 
-			if comment_from_cache and comment_from_cache != -1:
-				helper.logger.info('Getting comments from cache')
-				comments_selection = paginate_comments(args, comment_from_cache, block_size, selection, search_data)
-			else:
-				comment_list = get_comments(https, SUBDIVX_URL, id_subtitle)
-				cache_comments.put(id_subtitle, comment_list)
+            if comment_from_cache and comment_from_cache != -1:
+                helper.logger.info('Getting comments from cache')
+                comments_selection = paginate_comments(args, comment_from_cache, block_size, selection, search_data)
+            else:
+                comment_list = get_comments(https, SUBDIVX_URL, id_subtitle)
+                cache_comments.put(id_subtitle, comment_list)
 
-				if comment_list:
-					comments_selection = paginate_comments(args, comment_list, block_size, selection, search_data)
+                if comment_list:
+                    comments_selection = paginate_comments(args, comment_list, block_size, selection, search_data)
 
-		# Show selection menu
-		user_input = prompt_user_selection(args, 'download') if comments_selection is None else comments_selection
+        # Show selection menu
+        user_input = prompt_user_selection(args, 'download') if comments_selection is None else comments_selection
 
-		try:
-			select_action = int(user_input)
-		except ValueError:
-			print('\nInput only numbers')
-			delay(0)
-			continue
+        try:
+            select_action = int(user_input)
+        except ValueError:
+            print('\nInput only numbers')
+            delay(0)
+            continue
 
-		if select_action > 1 or select_action <= -1:
-			print('\nInput valid numbers')
-			delay(0)
-			continue
-		elif select_action == 1:
-			clear()
-			get_subtitle(args, https, SUBDIVX_URL, id_subtitle)
-			if args.no_exit:
-				continue
-			exit(0)
-		elif select_action == 0:
-			clear()
-			exit(0)
+        if select_action > 1 or select_action <= -1:
+            print('\nInput valid numbers')
+            delay(0)
+            continue
+        elif select_action == 1:
+            clear()
+            get_subtitle(args, https, SUBDIVX_URL, id_subtitle)
+            if args.no_exit:
+                continue
+            exit(0)
+        elif select_action == 0:
+            clear()
+            exit(0)
 
 if __name__ == '__main__':
-	main()
+    main()
